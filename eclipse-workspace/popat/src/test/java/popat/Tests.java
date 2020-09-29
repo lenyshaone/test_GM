c50e2ff1f1062ac8b0f24fa9af68120e1604242f
Elena <lenyshaone@gmail.com>
2020-09-29 13:52:01 +0300

    testing_gmail

--- eclipse-workspace/popat/src/test/java/popat/Tests.java
+++ eclipse-workspace/popat/src/test/java/popat/Tests.java
@@ -1,67 +1,69 @@
 package popat;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
-import org.openqa.selenium.By;
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.Step;
+import io.qameta.allure.Story;
 
 
 
 public class Tests {
 	
 	WebDriver driver;
-	int len;
+	String nodeUrl;
+
 	
 	@BeforeClass
-	public void setup()
-	{		
-		WebDriverManager.chromedriver().setup();
-		driver = new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);		
-		driver.get("https://gmail.com/");
-		driver.manage().window().maximize();		
-	}
-	
-	@Test(priority=1)
-	public void logoPresens()
-	{		
-		WebElement username = driver.findElement(By.xpath("//input[@type='email']"));
-		username.sendKeys("testgmln@gmail.com");		 		  
-	    driver.findElement(By.id("identifierNext")).click();		  		  
-	}
-	
-	@Test(priority=2)
-	public void loginTest()
-	{
-		WebElement pwd = driver.findElement(By.xpath("//input[@name='password']"));		
-		pwd.sendKeys("smbtestgm");		  
-		driver.findElement(By.id("passwordNext")).click();
-			
-	}
-	
-	@Test(priority=3)
-	public void registrationTest() throws InterruptedException
-	{
-		String name2find = "Елена Арсланова";			 
-		len = driver.findElements(By.cssSelector("div.afn [name='"+name2find+"']")).size();				 
-		driver.findElement(By.xpath("//div[@class='T-I T-I-KE L3']")).click();
-		WebElement to = driver.findElement(By.xpath("//textarea[@name='to']"));			  
-		to.sendKeys("testgmln@gmail.com");
-	    WebElement sub = driver.findElement(By.xpath("//input[@name='subjectbox']"));
-		sub.sendKeys("Тестовое задание. <Арсланова>");
-		WebElement txt = driver.findElement(By.xpath("//div[@class='Am Al editable LW-avf tS-tW']"));
-		txt.sendKeys(String.valueOf(len)+" letters from: "+ name2find);
-		driver.findElement(By.xpath("//div[@class='T-I J-J5-Ji aoO v7 T-I-atl L3']")).click();
-		Thread.sleep(2000);					 			 						  
-	}
+	public void setup() throws MalformedURLException
+	{	nodeUrl="http://192.168.0.151:24589/wd/hub";
+		DesiredCapabilities cap =DesiredCapabilities.chrome();
+		cap.setBrowserName("chrome");
+		cap.setPlatform(Platform.WINDOWS);
 		
+		driver = new RemoteWebDriver(new URL(nodeUrl), cap);
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		
+		driver.get("https://gmail.com/");
+		String currenTitle = driver.getTitle();
+		String expectedTitle = "Gmail";
+		Assert.assertEquals(currenTitle, expectedTitle);
+	
+	}
+	
+	@Test(priority=1, description="Check sending message")
+	@Description("Check sending messag.......")
+	@Epic("EPIC001")
+	@Feature("Feature1: Send email")
+	@Story("Story: Email send")
+	@Step("Verification of sending Email")
+	@Severity(SeverityLevel.BLOCKER)
+	public void clickEmail()
+	{
+		
+		GmailPageObjects gp=PageFactory.initElements(driver, GmailPageObjects.class);
+		gp.enterEmail("testgmln@gmail.com");//Replace with your email id
+		gp.enterPassword("smbtestgm");//Replace with your password
+		gp.sendEmail("Елена Арсланова","testgmln@gmail.com");//Replace with email subject you want to click
+
+	}
+	
 	@AfterClass
 	public void tesrDown() 
 	{			
